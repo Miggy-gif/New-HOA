269edfef46ca244b09960d8f176e1de4823b9b13
Josol <115700033+Miggy-gif@users.noreply.github.com>
2024-01-31 23:27:51 +0800

    updated pages and fixed error on App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Components/Sidebar';
 import Header from './header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Update import statement
 import Dashboard from './Pages/Dashboard';
+import Dues from './Pages/Dues';
+import Notification from './Pages/Notification';
+
 
 
 function App() {
   return (
     <div className="App">
+       <Router>
+        <Sidebar>
+          <Routes> {/* Use Routes instead of Switch */}
+            <Route path='/dashboard' element={Dashboard} />
+            <Route path='/dues' element={Dues} />
+            <Route path='/notification' element={Notification} />
+          </Routes>
+        </Sidebar>
+      </Router>
         <Header/>
-        <Sidebar/>
-        <Dashboard/>
+        
     </div>
   );
 }
